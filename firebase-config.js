615ba7b18f68a50cc19c407cae65d121cc2b6587
Noor Fatima Healthcare Group <WarMohammad@hotmail.com>
2025-08-02 19:07:44 +0530

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
+// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBZi12tqCNYaGHENKuunmZ8ZJ5i1W4tQSE",
-  authDomain: "noor-fatima-healthcare-group.firebaseapp.com",
-  databaseURL: "https://noor-fatima-healthcare-group-default-rtdb.firebaseio.com",
-  projectId: "noor-fatima-healthcare-group",
-  storageBucket: "noor-fatima-healthcare-group.appspot.com",
-  messagingSenderId: "251108193993",
-  appId: "1:251108193993:web:e09f8ba7d8899725a87f18"
+  apiKey: "AIzaSyB4VUfZ5k0UbrR0TB11ogJm6a-JmPGANHA",
+  authDomain: "noorfatimahealthcaregrou-4419e.firebaseapp.com",
+  databaseURL: "https://noorfatimahealthcaregrou-4419e-default-rtdb.firebaseio.com",
+  projectId: "noorfatimahealthcaregrou-4419e",
+  storageBucket: "noorfatimahealthcaregrou-4419e.firebasestorage.app",
+  messagingSenderId: "608595978890",
+  appId: "1:608595978890:web:a545246db9317ea3745b73",
+  measurementId: "G-DPECH577P8"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db };
+export const db = getFirestore(app);
